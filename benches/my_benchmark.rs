6ef19d89f0a6024ff09bf705bc1220771420e418
Ryan P. Brewster <RyanPBrewster@gmail.com>
2024-11-11 11:50:11 -0500

    Demo for low-cardinality tree

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,22 +1,39 @@
 use std::collections::BTreeMap;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use rule_match::{bag, LinearScan, Matcher, Rule};
+use rule_match::{bag, LinearScan, LowCardinalityTree, Matcher, Rule};
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let rules: Vec<Rule> = itertools::iproduct!(0..10, 0..10, 0..10).map(|(i, j, k)| {
-        Rule(BTreeMap::from([
-            ("a".to_owned(), i.to_string()),
-            ("b".to_owned(), j.to_string()),
-            ("c".to_owned(), k.to_string()),
-        ]))
-    }).collect();
-    let m = LinearScan::new(rules);
+fn make_rules() -> Vec<Rule> {
+    itertools::iproduct!(0..10, 0..10, 0..10).map(|(i, j, k)| {
+        let mut r = BTreeMap::new();
+        if i > 0 {
+            r.insert("a".to_owned(), i.to_string());
+        }
+        if j > 0 {
+            r.insert("b".to_owned(), j.to_string());
+        }
+        if k > 0 {
+            r.insert("c".to_owned(), k.to_string());
+        }
+        Rule(r)
+    }).skip(1).collect()
+}
+
+fn linear_scan(c: &mut Criterion) {
+    let m = LinearScan::new(make_rules());
 
     c.bench_function("no_match", |b| b.iter(|| {
         m.find(black_box(bag! { "a" => "garbage", "b" => "garbage", "c" => "garbage" }))
     }));
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn low_cardinality_tree(c: &mut Criterion) {
+    let m = LowCardinalityTree::new(make_rules());
+
+    c.bench_function("no_match", |b| b.iter(|| {
+        m.find(black_box(bag! { "a" => "garbage", "b" => "garbage", "c" => "garbage" }))
+    }));
+}
+
+criterion_group!(benches, linear_scan, low_cardinality_tree);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 
 pub struct Rule(pub BTreeMap<String, String>);
 
@@ -35,6 +35,90 @@ impl Matcher for LinearScan {
     }
 }
 
+pub struct LowCardinalityTree {
+    keys: Vec<String>,
+    root: Option<Node>,
+}
+#[derive(Debug)]
+enum Node {
+    Leaf(usize),
+    Parent {
+        wildcard: Option<Box<Node>>,
+        explicit: BTreeMap<String, Box<Node>>,
+    }
+}
+impl Matcher for LowCardinalityTree {
+    fn new(rules: Vec<Rule>) -> Self {
+        let keys: Vec<String> = {
+            let deduped = rules.iter().flat_map(|r| r.0.keys().cloned()).collect::<BTreeSet<_>>();
+            deduped.into_iter().collect()
+        };
+        if keys.is_empty() {
+            return Self {
+                keys,
+                root: if rules.is_empty() { None } else { Some(Node::Leaf(0)) },
+            };
+        }
+
+        let mut root = Node::Parent { wildcard: None, explicit: BTreeMap::new() };
+        for (rule_idx, rule) in rules.into_iter().enumerate() {
+            let mut cur = &mut root;
+            for (key_idx, key) in keys.iter().enumerate() {
+                let child = if key_idx == keys.len() - 1 {
+                    Node::Leaf(rule_idx)
+                } else {
+                    Node::Parent { wildcard: None, explicit: BTreeMap::new() }
+                };
+                match cur {
+                    Node::Leaf(_) => unreachable!("no interior leaves"),
+                    Node::Parent { wildcard, explicit } => {
+                        match rule.0.get(key) {
+                            None => {
+                                cur = wildcard.get_or_insert_with(|| Box::new(child));
+                            }
+                            Some(value) => {
+                                cur = explicit.entry(value.to_owned()).or_insert(Box::new(child));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Self { keys, root: Some(root) }
+    }
+
+    fn find(&self, input: BTreeMap<String, String>) -> Option<usize> {
+        find_min_leaf(&self.keys, self.root.as_ref()?, &input)
+    }
+}
+
+fn find_min_leaf(keys: &[String], node: &Node, input: &BTreeMap<String, String>) -> Option<usize> {
+    match node {
+        Node::Leaf(idx) => Some(*idx),
+        Node::Parent { wildcard, explicit } => {
+            let mut best = None;
+            if let Some(child) = wildcard {
+                best = min_some(best, find_min_leaf(&keys[1..], &child, input));
+            }
+            if let Some(value) = input.get(&keys[0]) {
+                if let Some(child) = explicit.get(value) {
+                    best = min_some( best, find_min_leaf(&keys[1..], child, input));
+                }
+            }
+            best
+        }
+    }
+}
+
+fn min_some(a: Option<usize>, b: Option<usize>) -> Option<usize> {
+    match (a, b) {
+        (None, None) => None,
+        (None, Some(b)) => Some(b),
+        (Some(a), None) => Some(a),
+        (Some(a), Some(b)) => Some(std::cmp::min(a, b)),
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -43,6 +127,20 @@ mod tests {
     #[test]
     fn linear_scan_smoke() {
         let m = LinearScan::new(vec![
+            Rule(bag!{ "a" => "1", "b" => "2" }),
+            Rule(bag!{ "a" => "1" }),
+            Rule(bag!{ "b" => "2" }),
+        ]);
+
+        assert_eq!(m.find(bag! { "a" => "1", "b" => "2", "c" => "3" }), Some(0));
+        assert_eq!(m.find(bag! { "a" => "1", "b" => "garbage", "c" => "3" }), Some(1));
+        assert_eq!(m.find(bag! { "a" => "garbage", "b" => "2", "c" => "3" }), Some(2));
+        assert_eq!(m.find(bag! { "a" => "garbage", "b" => "garbage", "c" => "3" }), None);
+    }
+
+    #[test]
+    fn low_cardinality_tree_smoke() {
+        let m = LowCardinalityTree::new(vec![
             Rule(bag!{ "a" => "1", "b" => "2" }),
             Rule(bag!{ "a" => "1" }),
             Rule(bag!{ "b" => "2" }),
